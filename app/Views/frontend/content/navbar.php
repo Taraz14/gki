fe3b7a78cfe12f4341af2eda3d41578f2e19a5d1
Dante <bonaventura.pradhana@students.amikom.ac.id>
2024-09-28 18:20:06 +0900

    new base GKI

---
+++ app/Views/frontend/content/navbar.php
@@ -0,0 +1,56 @@
+<header id="header">
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col-md-2">
+                <div class="social-links">
+                    <ul>
+                        <li>
+                            <a href="#"><i class="icon icon-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="icon icon-twitter"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="icon icon-youtube-play"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="icon icon-behance-square"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-10">
+                <nav id="navbar">
+                    <div class="main-menu stellarnav">
+                        <ul class="menu-list">
+                            <li class="menu-item active"><a href="#home">Home</a></li>
+                            <li class="menu-item"><a href="#featured-books" class="nav-link">Sejarah</a></li>
+                            <li class="menu-item"><a href="#popular-books" class="nav-link">Visi & Misi</a></li>
+                            <li class="menu-item"><a href="#special-offer" class="nav-link">Struktur Gereja</a></li>
+                            <li class="menu-item"><a href="#latest-blog" class="nav-link">Informasi Pelayanan</a></li>
+                            <!-- <li class="menu-item"><a href="#download-app" class="nav-link">Download App</a></li> -->
+                            <li class="menu-item has-sub">
+                                <a href="#pages" class="nav-link">Kegiatan Gereja</a>
+                                <ul>
+                                    <li class="active"><a href="index.html">HUT Gereja</a></li>
+                                    <li><a href="index.html">HUT YPK</a></li>
+                                    <li><a href="index.html">HUT PI</a></li>
+                                </ul>
+
+                            </li>
+                        </ul>
+
+                        <div class="hamburger">
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                        </div>
+
+                    </div>
+                </nav>
+
+            </div>
+        </div>
+    </div>
+</header>
